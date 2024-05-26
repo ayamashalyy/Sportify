df3cbc57e6ab5e234382d162e190c659743eb51e
MarimYacout <marim.yacout@gmail.com>
2024-05-27 00:19:41 +0300

    test not done yet

--- SportifyTests/FavoriteViewModelTests.swift
+++ SportifyTests/FavoriteViewModelTests.swift
@@ -1,100 +1,87 @@
-////
-////  FavoriteViewModelTests.swift
-////  SportifyTests
-////
-////  Created by Marim Mohamed Mohamed Yacout on 26/05/2024.
-////
 //
+//  FavoriteViewModelTests.swift
+//  SportifyTests
+//
+//  Created by Marim Mohamed Mohamed Yacout on 26/05/2024.
+//
+
 //import XCTest
 //@testable import Sportify
 //
-//// Mock DatabaseManager to simulate database interactions
-//class MockDataBaseManager: DataBaseManager {
-//    var leagues: [LegueModel] = []
-//
-//    override func getAllLeagues() -> [LegueModel] {
-//        return leagues
-//    }
-//
-//    override func deleteLeagueFromFavorites(league: LegueModel) {
-//        if let index = leagues.firstIndex(where: { $0.id == league.id }) {
-//            leagues.remove(at: index)
-//        }
-//    }
-//}
-//
-//// Unit test class for FavoriteViewModel
 //class FavoriteViewModelTests: XCTestCase {
+//    
 //    var viewModel: FavoriteViewModel!
-//    var mockDataBaseManager: MockDataBaseManager!
-//
+//    var mockDatabaseManager: MockDataBaseManager!
+//    
 //    override func setUp() {
 //        super.setUp()
-//        mockDataBaseManager = MockDataBaseManager()
-//        DataBaseManager.shared = mockDataBaseManager
+//        mockDatabaseManager = MockDataBaseManager()
+//        swizzleDatabaseManagerShared()
 //        viewModel = FavoriteViewModel()
 //    }
-//
+//    
 //    override func tearDown() {
 //        viewModel = nil
-//        mockDataBaseManager = nil
+//        mockDatabaseManager = nil
 //        super.tearDown()
 //    }
-//
+//    
+//    func swizzleDatabaseManagerShared() {
+//        let originalSelector = #selector(getter: DataBaseManager.shared)
+//        let swizzledSelector = #selector(getter: mock_shared)
+//        
+//        let originalMethod = class_getClassMethod(DataBaseManager.self, originalSelector)
+//        let swizzledMethod = class_getInstanceMethod(FavoriteViewModelTests.self, swizzledSelector)
+//        
+//        method_exchangeImplementations(originalMethod!, swizzledMethod!)
+//    }
+//    
+//    @objc var mock_shared: DataBaseManager {
+//        return mockDatabaseManager as! DataBaseManager
+//    }
+//    
 //    func testFetchLeaguesFromDB() {
 //        // Given
-//        let league1 = LegueModel(id: 1, name: "League 1")
-//        let league2 = LegueModel(id: 2, name: "League 2")
-//        mockDataBaseManager.leagues = [league1, league2]
-//
-//        let expectation = self.expectation(description: "leaguesDidChange called")
-//
-//        viewModel.leaguesDidChange = {
-//            expectation.fulfill()
-//        }
-//
+//        let league = LegueModel(league_key: 1, league_name: "Test League", league_logo: "logo_url")
+//        mockDatabaseManager.insertLeague(league: league)
+//        
 //        // When
 //        viewModel.fetchLeaguesFromDB()
-//
+//        
 //        // Then
-//        waitForExpectations(timeout: 1, handler: nil)
-//        XCTAssertEqual(viewModel.leagues.count, 2)
-//        XCTAssertEqual(viewModel.leagues[0].league_name, "League 1")
-//        XCTAssertEqual(viewModel.leagues[1].league_name, "League 2")
+//        XCTAssertEqual(viewModel.leagues.count, 1)
+//        XCTAssertEqual(viewModel.leagues.first?.league_name, "Test League")
 //    }
-//
+//    
 //    func testDeleteLeague() {
 //        // Given
-//        let league1 = LegueModel(id: 1, name: "League 1")
-//        let league2 = LegueModel(id: 2, name: "League 2")
-//        mockDataBaseManager.leagues = [league1, league2]
-//
-//        let expectation = self.expectation(description: "leaguesDidChange called")
-//
+//        let league1 = LegueModel(league_key: 1, league_name: "Test League 1", league_logo: "logo_url_1")
+//        let league2 = LegueModel(league_key: 2, league_name: "Test League 2", league_logo: "logo_url_2")
+//        mockDatabaseManager.insertLeague(league: league1)
+//        mockDatabaseManager.insertLeague(league: league2)
+//        
+//        // When
+//        viewModel.deleteLeague(league: league1)
+//        
+//        // Then
+//        XCTAssertEqual(viewModel.leagues.count, 1)
+//        XCTAssertEqual(viewModel.leagues.first?.league_name, "Test League 2")
+//    }
+//    
+//    func testLeaguesDidChangeCallback() {
+//        // Given
+//        let expectation = self.expectation(description: "Leagues did change callback")
 //        viewModel.leaguesDidChange = {
 //            expectation.fulfill()
 //        }
-//
+//        
+//        let league = LegueModel(league_key: 1, league_name: "Test League", league_logo: "logo_url")
+//        mockDatabaseManager.insertLeague(league: league)
+//        
 //        // When
-//        viewModel.deleteLeague(league: league1)
-//
+//        viewModel.fetchLeaguesFromDB()
+//        
 //        // Then
-//        waitForExpectations(timeout: 1, handler: nil)
-//        XCTAssertEqual(viewModel.leagues.count, 1)
-//        XCTAssertEqual(viewModel.leagues[0].league_name, "League 2")
+//        waitForExpectations(timeout: 1.0, handler: nil)
 //    }
-//}
-//
-//// Dummy implementation to make the code compile
-//// Replace this with the actual implementation of DataBaseManager in your project
-//class DataBaseManager {
-//    static var shared = DataBaseManager()
-//    func getAllLeagues() -> [LegueModel] { return [] }
-//    func deleteLeagueFromFavorites(league: LegueModel) {}
-//}
-//
-//// Ensure this is replaced with the actual LegueModel from your main project
-//struct LegueModel: Equatable {
-//    var id: Int
-//    var name: String
 //}
